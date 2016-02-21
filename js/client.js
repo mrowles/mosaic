210da080241d20afcf8b981e7e61dc96d5fbe890
Matt Rowles <matthewrowles@gmail.com>
2016-02-21 23:57:38 +1100

    Refactor code - removes some unnecessary spaghetti - adds some comments

--- js/client.js
+++ js/client.js
@@ -2,10 +2,9 @@ var Mosaic = (function () {
 
   'use strict';
 
-  var $fileUploader = document.getElementById('image-uploader');
   var $canvasMosaic = document.getElementById('canvas-mosaic');
+  var $fileUploader = document.getElementById('image-uploader');
   var canvasMosaicCtx = $canvasMosaic.getContext('2d');
-  //var colourRows = [];
 
   /**
    * Mosaic
@@ -15,7 +14,23 @@ var Mosaic = (function () {
     this.max = 0;
   }
 
-  // Convert an RGB value to Hex
+  /**
+   * Convert an RGB component to hex
+   * @param c - RGB component
+   * @returns {string}
+   */
+  function convertRgbComponentToHex(c) {
+    var hex = c.toString(16);
+    return hex.length === 1 ? '0' + hex : hex;
+  }
+
+  /**
+   * Convert an RGB value to Hex
+   * @param r - red hex valie
+   * @param g - green hex value
+   * @param b - blue hex value
+   * @returns {string}
+   */
   function convertRgbToHex(r, g, b) {
     var hex = '#FFFFFF';
 
@@ -28,13 +43,11 @@ var Mosaic = (function () {
     return hex;
   }
 
-  // Convert an RGB component to hex
-  function convertRgbComponentToHex(c) {
-    var hex = c.toString(16);
-    return hex.length === 1 ? '0' + hex : hex;
-  }
-
-  // Compute average RGB of given Canvas data
+  /**
+   * Compute average RGB of given Canvas data
+   * @param imgData - image data pulled from canvas
+   * @returns {{r: number, g: number, b: number}}
+   */
   function getAverageRgbOfCanvas(imgData) {
     var BLOCKSIZE = 1;
     var DATA_LEN = imgData.data.length;
@@ -78,38 +91,47 @@ var Mosaic = (function () {
     };
   }
 
-  function TileRow(row, preloader, index) {
-    preloader.queue(row)
-      .then(function () {
-        console.log('Row ' + index + ' loaded.');
-      }, function (err) {
-        console.log('Error:', err);
-      });
+  /**
+   * Builds service information related to each tile
+   * @param canvasContext - context of canvas sourcing tile information for
+   * @param height - height of image
+   * @param width - width of image
+   * @returns {Array} of tile rows
+   */
+  function buildTileServices(canvasContext, height, width) {
+    var imgData;
+    var rgb = '';
+    var hex = '';
+    var service = '';
+    var mosaicRow = [];
+    var tiles = [];
 
-  }
+    for (var col = 0; (col + TILE_WIDTH) < height; col += TILE_WIDTH) {
+      mosaicRow = [];
 
-  // TODO
-  // Needs comments
-  function workMagic(tiles) {
-    tiles.forEach(function (array, index) {
-      new TileRow(array, mosaic, index);
-    });
+      for (var row = 0; (row + TILE_HEIGHT) < width; row += TILE_HEIGHT) {
 
-    mosaic.preload().then(function (tiles) {
-      console.log('All mosaic tiles loaded:', tiles.length);
-    }, function (err) {
-      console.log('Error loading mosaic tiles:', err);
-    });
+        imgData = canvasContext.getImageData(row, col, TILE_WIDTH, TILE_HEIGHT);
+
+        rgb = getAverageRgbOfCanvas(imgData);
+        hex = convertRgbToHex(rgb.r, rgb.g, rgb.b);
+        service = '/color/' + hex.split('#')[1];
+
+        mosaicRow.push(service);
+      }
+
+      tiles.push(mosaicRow);
+    }
+
+    return tiles;
   }
 
   /**
-   * The `queue` methods is intended to add an array (a deck) of images to the
-   * queue. It does not preload the images though; only adds them to the queue.
-   * @param  {Array} array - Array of images to add the queue
-   * @return {Promise}
+   * Queue up array of items for processing
+   * @param array - array of items for deferring
+   * @returns {Promise}
    */
-  Mosaic.prototype.queue = function (array) {
-
+  Mosaic.prototype.queueItems = function (array) {
     if (!Array.isArray(array)) {
       array = [array];
     }
@@ -128,15 +150,83 @@ var Mosaic = (function () {
   };
 
   /**
-   * The `preloadImage` preloads the image resource living at `path` and returns
-   * a promise that resolves when the image is successfully loaded by the
-   * browser or if there is an error. Beware, the promise is not rejected in
-   * case the image cannot be loaded; it gets resolved nevertheless.
-   * @param  {String} path - Image url
-   * @return {Promise}
+   * Preload collection of tiles for processing
+   */
+  Mosaic.prototype.preloadTiles = function () {
+    for (var i = 0; i < this.max; i++) {
+      this.items.forEach(function (item) {
+        if (typeof item.collection[i] !== 'undefined') {
+          item.collection[i] = this.preloadImage(item.collection[i]);
+        }
+      }, this);
+    }
+  };
+
+  /**
+   * Draw resolved rows of tiles on canvas
+   */
+  Mosaic.prototype.drawTiles = function () {
+    var x = 0;
+    var y = 0;
+
+    this.items.forEach(function (item) {
+
+      Promise.all(item.collection).then(
+        // success
+        function (row) {
+          x = 0;
+
+          row.forEach(function (img) {
+            var path = img.path ? img.path[0] : img.target;
+            canvasMosaicCtx.drawImage(path, x, y, TILE_WIDTH, TILE_HEIGHT);
+            x = x + TILE_HEIGHT;
+          });
+
+          y = y + TILE_WIDTH;
+        },
+        // error
+        function (err) {
+          console.error('Error resolving all item promises: ', err);
+        }
+      );
+
+    });
+  };
+
+  /**
+   *  Load a file from the fileuploaded
+   */
+  Mosaic.prototype.loadFile = function () {
+    return new Promise(function (resolve) {
+      var fileUpload = new FileReader();
+
+      if ($fileUploader.files.length < 1) {
+        alert('Please supply a file');
+        console.error('No file supplied');
+        return;
+      }
+
+      var image = $fileUploader.files[0];
+
+      if (image.type.indexOf('image') === -1) {
+        alert('Please supply an image file (png, gif, jpg, bmp');
+        console.error('Not an image supplied');
+        return;
+      }
+
+      fileUpload.onload = resolve;
+      fileUpload.onerror = resolve;
+      fileUpload.readAsDataURL(image);
+    });
+  };
+
+  /**
+   * Preload an image
+   * @param path - path of image to preload
+   * @returns {Promise}
    */
   Mosaic.prototype.preloadImage = function (path) {
-    return new Promise(function (resolve, reject) {
+    return new Promise(function (resolve) {
       var image = new Image();
       image.onload = resolve;
       image.onerror = resolve;
@@ -145,81 +235,11 @@ var Mosaic = (function () {
   };
 
   /**
-   * The `preload` method preloads the whole queue of decks added through the
-   * `queue` method. It returns a promise that gets resolved when all decks have
-   * been fully loaded.
-   * The decks are loaded either sequencially (one after the other) or in
-   * parallel, depending on the `parallel` options.
-   * @return {Promise}
+   * Creates the mosaic information
+   * @param image - image to create mosaic data about
+   * @returns {*} - array of tile information
    */
-  Mosaic.prototype.preload = function () {
-    var tile;
-    var tiles = [];
-
-    var x = 0;
-    var y = 0;
-
-    for (var i = 0; i < this.max; i++) {
-      this.items.forEach(function (item) {
-        if (typeof item.collection[i] !== 'undefined') {
-          item.collection[i] = this.preloadImage(item.collection[i]);
-        }
-      }, this);
-    }
-
-    this.items.forEach(function (item) {
-
-      tile = Promise.all(item.collection).then(function (item) {
-        x = 0;
-
-        item.forEach(function (img) {
-          var path = img.path ? img.path[0] : img.target;
-          canvasMosaicCtx.drawImage(path, x, y, TILE_WIDTH, TILE_HEIGHT);
-          x = x + TILE_HEIGHT;
-        });
-
-        y = y + TILE_WIDTH;
-      }, function (err) {
-        console.log('error: ', err);
-      });
-
-      tiles.push(tile);
-    });
-
-    return Promise.all(tiles);
-  };
-
-  // Builds service information related to each tile
-  function buildTileServices(canvasContext, height, width) {
-    var imgData;
-    var rgb = '';
-    var hex = '';
-    var service = '';
-    var colourRow = [];
-    var tiles = [];
-
-    for (var col = 0; (col + TILE_WIDTH) < height; col += TILE_WIDTH) {
-      colourRow = [];
-
-      for (var row = 0; (row + TILE_HEIGHT) < width; row += TILE_HEIGHT) {
-
-        imgData = canvasContext.getImageData(row, col, TILE_WIDTH, TILE_HEIGHT);
-
-        rgb = getAverageRgbOfCanvas(imgData);
-        hex = convertRgbToHex(rgb.r, rgb.g, rgb.b);
-        service = '/color/' + hex.split('#')[1];
-
-        colourRow.push(service);
-      }
-
-      tiles.push(colourRow);
-    }
-
-    return tiles;
-  }
-
-  // Creates the mosaic information
-  function createMosaicData(image) {
+  Mosaic.prototype.createMosaicData = function (image) {
     var width = image.width;
     var height = image.height;
 
@@ -232,71 +252,32 @@ var Mosaic = (function () {
 
     var inMemoryCanvasCtx = $canvasEl.getContext('2d');
     inMemoryCanvasCtx.drawImage(image, 0, 0);
-    inMemoryCanvasCtx.save();
 
     var tiles = buildTileServices(inMemoryCanvasCtx, height, width);
 
     return tiles;
-  }
-
-  // Upload a file
-  // TODO
-  // Needs work, should return promise
-  function loadFile() {
-    var fileUpload = new FileReader();
-
-    if ($fileUploader.files.length < 1) {
-      console.log('No file supplied');
-      return;
-    }
-
-    var image = $fileUploader.files[0];
-
-    if (image.type.indexOf('image') === -1) {
-      console.log('Not an image supplied');
-      return;
-    }
-
-    fileUpload.onload = function (e) {
-      var source = e.target.result;
-      loadImage(source);
-    };
-
-    fileUpload.onerror = function (e) {
-      console.log('Error reading file, please try again');
-    };
-
-    fileUpload.readAsDataURL(image);
-  }
-
-  // Load an image
-  // TODO
-  // Needs work, should return promise
-  function loadImage(src) {
-    var image = new Image();
-
-    image.onload = function () {
-      var tiles = createMosaicData(image);
-      workMagic(tiles);
-    };
-
-    image.onerror = function () {
-      console.log('Error loading image, please try again');
-    };
-
-    image.src = src;
-  }
-
-  Mosaic.prototype.loadFile = function () {
-    return loadFile.call(this);
   };
 
-  Mosaic.prototype.loadImage = function (src) {
-    return loadImage.call(this, src);
-  };
+  /**
+   * Builds mosaic based on image processing
+   * @param tiles - tiles to queue, preload and draw
+   */
+  Mosaic.prototype.buildMosaic = function (tiles) {
+    var self = this;
+    tiles.forEach(function (row) {
+      self.queueItems(row).then(
+        // success
+        function () {
 
-  Mosaic.prototype.createMosaic = function (image) {
-    return createMosaic().call(this, image)
+        },
+        // error
+        function (err) {
+          console.error('Error queueing up tile:', err);
+        });
+    });
+
+    self.preloadTiles();
+    self.drawTiles();
   };
 
   return Mosaic;
@@ -305,4 +286,35 @@ var Mosaic = (function () {
 
 var mosaic = new Mosaic();
 
+/**
+ * The mosaic function which processes the uploaded image into a mosaic
+ * Note: known Safari bug, will fix
+ */
+function processImageToMosaic() {
 
+  'use strict';
+
+  mosaic.loadFile(this).then(
+    // success
+    function (file) {
+      var imgFile = file.target.result;
+
+      mosaic.preloadImage(imgFile).then(
+        //success
+        function (img) {
+          var imgPath = img.path ? img.path[0] : img.target;
+          var tiles = mosaic.createMosaicData(imgPath);
+          mosaic.buildMosaic(tiles);
+        },
+        //error
+        function (err) {
+          console.error('Error preloading image: ', err);
+        }
+      );
+    },
+    // error
+    function (err) {
+      console.error('Error loading load: ', err);
+    }
+  );
+}
